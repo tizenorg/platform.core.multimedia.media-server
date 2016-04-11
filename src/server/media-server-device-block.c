01bdb1a8aca3062f40947ac05c9b161cc66e1722
Minje Ahn <minje.ahn@samsung.com>
2016-04-12 08:08:00 +0900

    Fix svace issue (WGID-72147)
    Change-Id: Iccb42c9141069f4dbf15b765406b3303a34f5210
    Signed-off-by: Minje Ahn <minje.ahn@samsung.com>

--- src/server/media-server-device-block.c
+++ src/server/media-server-device-block.c
@@ -147,7 +147,7 @@ int ms_usb_remove_handler(const char *mount_path, const char *mount_uuid)
 		ms_disconnect_db(&handle);
 
 		ms_unload_functions();
-  	} else if (strncmp(mount_path, USB_STORAGE_REMOVED, strlen(USB_STORAGE_REMOVED)) == 0 && mount_uuid  == NULL) {
+	} else if (mount_path != NULL && strncmp(mount_path, USB_STORAGE_REMOVED, strlen(USB_STORAGE_REMOVED)) == 0) {
 		ret = ms_load_functions();
 		if (ret != MS_MEDIA_ERR_NONE) {
 			MS_DBG_ERR("ms_load_functions failed [%d]", ret);
